03b444debe1be0a92ce410c12e3619c6a84b4751
heungno <gmd5366@gmail.com>
2020-06-16 13:48:44 +0900

    Updates

---
+++ precache-manifest.6d6617192eb274c7c826bdc5c33085d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1d344f2db6bd2a08b6030a40713492",
+    "url": "/heung.win/frontend/index.html"
+  },
+  {
+    "revision": "59f6cbc7eec0e2d76672",
+    "url": "/heung.win/frontend/static/css/main.2c187ee7.chunk.css"
+  },
+  {
+    "revision": "af89495e326f689b9902",
+    "url": "/heung.win/frontend/static/js/2.23b49445.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/heung.win/frontend/static/js/2.23b49445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f6cbc7eec0e2d76672",
+    "url": "/heung.win/frontend/static/js/main.7f781534.chunk.js"
+  },
+  {
+    "revision": "e9c6a262bc8ee96c2628",
+    "url": "/heung.win/frontend/static/js/runtime-main.8714a20c.js"
+  }
+]);
